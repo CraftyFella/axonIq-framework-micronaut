c5f4f8242d2869a5068cfbfb267ee13f7d2e9099
Dave Craft <david.craft@mnscorp.net>
2025-06-09 13:29:11 +0100

    Working example

--- src/main/kotlin/com/playground/Domain.kt
+++ src/main/kotlin/com/playground/Domain.kt
@@ -149,3 +149,69 @@ class FlightAggregate() {
     }
 
 }
+
+
+
+class FlightAggregate2() {
+    var state: FlightState = FlightState.Empty
+    @AggregateIdentifier var aggregateId: String? = null
+    val decider = FlightDecider()
+
+    @CommandHandler
+    @CreationPolicy(AggregateCreationPolicy.CREATE_IF_MISSING)
+    fun handle(command: FlightCommand): String {
+        val events = decider.decide(state, command)
+        events.forEach { event ->
+            AggregateLifecycle.apply(event)
+        }
+        return events.toString()
+    }
+
+    @EventSourcingHandler
+    fun on(event: FlightEvent) {
+        this.aggregateId = event.flightId
+        this.state = this.state.evolve(event)
+    }
+}
+
+class FlightDecider {
+
+    fun decide(state: FlightState, command: FlightCommand): List<FlightEvent> {
+        return when (command) {
+            is FlightCommand.ScheduleFlightCommand -> handle(state, command)
+            is FlightCommand.DelayFlightCommand -> handle(state, command)
+            is FlightCommand.CancelFlightCommand -> handle(state, command)
+        }
+    }
+
+    private fun handle(
+        state: FlightState,
+        command: FlightCommand.ScheduleFlightCommand
+    ): List<FlightEvent> {
+        return if (state is FlightState.EmptyFlight) {
+            listOf(FlightEvent.FlightScheduledEvent(command.flightId, command.origin, command.destination))
+        } else {
+            listOf()
+        }
+    }
+
+    private fun handle(
+        state: FlightState,
+        command: FlightCommand.DelayFlightCommand
+    ): List<FlightEvent> {
+        if (state is FlightState.CancelledFlight) {
+            throw IllegalStateException("Flight already cancelled")
+        }
+        return listOf(FlightEvent.FlightDelayedEvent(command.flightId, command.reason))
+    }
+
+    private fun handle(
+        state: FlightState,
+        command: FlightCommand.CancelFlightCommand
+    ): List<FlightEvent> {
+        if (state is FlightState.CancelledFlight) {
+            throw IllegalStateException("Flight already cancelled")
+        }
+        return listOf(FlightEvent.FlightCancelledEvent(command.flightId, command.reason))
+    }
+}
